380cded2e6346348247b42062162e67b733b4f13
dchaboyer <dcchaboyer@mta.ca>
2016-11-22 15:13:55 -0400

    Drew's rough draft for concert methods

---
+++ addconcert.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Add Concert</title>
+</head>
+<body>
+<form action="http://localhost/concertadded.php" method="post">
+
+<b>Add a New Concert</b>
+
+<p>Name:
+<input type="text" name="name" size="30" value="" />
+</p>
+
+<p>Date:
+<input type="text" name="date" size="30" value="" />
+</p>
+
+<p>Venue:
+<input type="text" name="venue" size="30" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
