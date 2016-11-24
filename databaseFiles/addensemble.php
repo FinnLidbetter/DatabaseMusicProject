a3654d7f5e3b2369bb04f886831dbecc2b818be0
dchaboyer <dcchaboyer@mta.ca>
2016-11-23 22:13:07 -0400

    Added some ensemble files.

---
+++ databaseFiles/addensemble.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Add Ensemble</title>
+</head>
+<body>
+<form action="http://localhost/DatabaseMusicProject/databaseFiles/ensembleadded.php" method="post">
+
+<b>Add a New Ensemble</b>
+
+<p>Name:
+<input type="text" name="name" size="30" value="" />
+</p>
+
+<p>Date:
+<input type="text" name="date" size="30" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
---
+++ databaseFiles/ensembleAdded.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<title>Add Ensemble</title>
+</head>
+<body>
+<?php
+
+if(isset($_POST['submit'])){
+    
+    $data_missing = array();
+    
+    if(empty($_POST['name'])){
+        // Adds name to array
+        $data_missing[] = 'Name';
+    } else {
+        // Trim white space from the name and store the name
+        $name = trim($_POST['name']);
+    }
+    if(empty($_POST['date'])){
+        // Adds name to array
+        $data_missing[] = 'Date';
+    } else{
+        // Trim white space from the name and store the name
+        $date = trim($_POST['date']);
+    }
+
+    if(empty($data_missing)){
+        
+        require_once('../mysqlConnect.php');
+        $stmt = mysqli_prepare($dbc, "INSERT INTO Ensembles (name, date) VALUES (?, ?)");
+        mysqli_stmt_bind_param($stmt, 'ss', $name,
+                               $date);
+        
+        mysqli_stmt_execute($stmt);     
+        $affected_rows = mysqli_stmt_affected_rows($stmt);
+        
+        if($affected_rows == 1){         
+            echo 'Ensemble Entered';
+            mysqli_stmt_close($stmt);         
+            mysqli_close($dbc);
+            
+        } else {           
+            echo 'Error Occurred<br />';
+            echo mysqli_error();        
+            mysqli_stmt_close($stmt);         
+            mysqli_close($dbc);         
+        }       
+    } else {      
+        echo 'You need to enter the following data<br />';     
+        foreach($data_missing as $missing){          
+            echo "$missing<br />";          
+        }        
+    }   
+}
+
+?>
+
+<form action="http://localhost/DatabaseMusicProject/databaseFiles/ensembleAdded.php" method="post">
+    
+    <b>Add a New Ensemble</b>
+    
+    <p>Name:
+<input type="text" name="name" size="30" value="" />
+</p>
+
+<p>Date:
+<input type="text" name="date" size="30" value="" />
+</p>
+
+<p>
+    <input type="submit" name="submit" value="Send" />
+</p>
+    
+</form>
+</body>
+</html>
