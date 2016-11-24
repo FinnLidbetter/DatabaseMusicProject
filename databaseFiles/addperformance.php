90ca1bd88fa89fc0ab29c263a72df8a284428942
dchaboyer <dcchaboyer@mta.ca>
2016-11-24 17:31:39 -0400

    Added basic Performance php files

---
+++ databaseFiles/addperformance.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Add Performance</title>
+</head>
+<body>
+<form action="http://localhost/DatabaseMusicProject/databaseFiles/performanceadded.php" method="post">
+
+<b>Add a New Performance</b>
+
+<p>Performance Title:
+<input type="text" name="performanceTitle" size="30" value="" />
+</p>
+
+<p>Sponsor:
+<input type="text" name="sponsor" size="30" value="" />
+</p>
+
+<p>Image File Path:
+<input type="text" name="imageFilePath" size="30" value="" />
+</p>
+
+<p>Recording File Path:
+<input type="text" name="recordingFilePath" size="30" value="" />
+</p>
+
+<p>Concert Name:
+<input type="text" name="concertName" size="30" value="" />
+</p>
+
+<p>Concert Date:
+<input type="text" name="concertDate" size="30" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
---
+++ databaseFiles/performanceAdded.php
@@ -0,0 +1,119 @@
+<html>
+<head>
+<title>Add Person</title>
+</head>
+<body>
+<?php
+
+if(isset($_POST['submit'])){
+    
+    $data_missing = array();
+    
+    if(empty($_POST['performanceTitle'])){
+        // Adds name to array
+        $data_missing[] = 'Performance Title';
+    } else {
+        // Trim white space from the name and store the name
+        $performanceTitle = trim($_POST['performanceTitle']);
+    }
+    if(empty($_POST['sponsor'])){
+        // Adds name to array
+        $data_missing[] = 'Sponsor';
+    } else{
+        // Trim white space from the name and store the name
+        $sponsor = trim($_POST['sponsor']);
+    }
+    if(empty($_POST['imageFilePath'])){
+        // Adds name to array
+        $data_missing[] = 'Image File Path';
+    } else {
+        // Trim white space from the name and store the name
+        $imageFilePath = trim($_POST['imageFilePath']);
+    }
+	
+	if(empty($_POST['recordingFilePath'])){
+        // Adds name to array
+        $data_missing[] = 'Recording File Path';
+    } else {
+        // Trim white space from the name and store the name
+        $recordingFilePath = trim($_POST['recordingFilePath']);
+    }
+    if(empty($_POST['concertName'])){
+        // Adds name to array
+        $data_missing[] = 'Concert Name';
+    } else{
+        // Trim white space from the name and store the name
+        $concertName = trim($_POST['concertName']);
+    }
+    if(empty($_POST['concertDate'])){
+        // Adds name to array
+        $data_missing[] = 'Concert Date';
+    } else {
+        // Trim white space from the name and store the name
+        $concertDate = trim($_POST['concertDate']);
+    }
+    if(empty($data_missing)){
+        
+        require_once('../mysqlConnect.php');
+        $stmt = mysqli_prepare($dbc, "INSERT INTO Performances(performanceTitle, sponsor, imageFilePath, recordingFilePath, concertName, concertDate) VALUES (?, ?, ?, ?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, 'ssssss', $performanceTitle, $sponsor, $imageFilePath, $recordingFilePath, $concertName, $concertDate);
+        
+        mysqli_stmt_execute($stmt);     
+        $affected_rows = mysqli_stmt_affected_rows($stmt);
+        
+        if($affected_rows == 1){         
+            echo 'Performance Entered';
+            mysqli_stmt_close($stmt);         
+            mysqli_close($dbc);
+            
+        } else {           
+            echo 'Error Occurred<br />';
+            echo mysqli_error();        
+            mysqli_stmt_close($stmt);         
+            mysqli_close($dbc);         
+        }       
+    } else {      
+        echo 'You need to enter the following data<br />';     
+        foreach($data_missing as $missing){          
+            echo "$missing<br />";          
+        }        
+    }   
+}
+
+?>
+
+<form action="http://localhost/DatabaseMusicProject/databaseFiles/performanceAdded.php" method="post">
+    
+    <b>Add a New Performance</b>
+    
+    <p>Performance Title:
+<input type="text" name="performanceTitle" size="30" value="" />
+</p>
+
+<p>Sponsor:
+<input type="text" name="sponsor" size="30" value="" />
+</p>
+
+<p>Image File Path:
+<input type="text" name="imageFilePath" size="30" value="" />
+</p>
+
+<p>Recording File Path:
+<input type="text" name="recordingFilePath" size="30" value="" />
+</p>
+
+<p>Concert Name:
+<input type="text" name="sponsor" size="30" value="" />
+</p>
+
+<p>Concert Date:
+<input type="text" name="concertDate" size="30" value="" />
+</p>
+
+<p>
+    <input type="submit" name="submit" value="Send" />
+</p>
+    
+</form>
+</body>
+</html>
