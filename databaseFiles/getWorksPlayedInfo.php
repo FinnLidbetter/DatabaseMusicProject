c723c1dc456abb3615313da589697d0cf73569eb
lgwood <woodlucas9@gmail.com>
2016-12-07 15:38:36 -0400

    changed works played to show ensemble performances

--- databaseFiles/getWorksPlayedInfo.php
+++ databaseFiles/getWorksPlayedInfo.php
@@ -7,11 +7,7 @@
   require_once('../config.php');
 
   // Create a query for the database
-<<<<<<< HEAD
   $query = "SELECT worksTitle, worksComposer, ensembleName, concertDate FROM ensembleWorksPlayed";
-=======
-  $query = "SELECT * FROM WorksPlayed";
->>>>>>> 27a2084de4cf1f7f5f937067d96ca0f46b53aaac
 
   // Get a response from the database by sending the connection
   // and the query
@@ -23,20 +19,10 @@
     echo '<table align="left"
     cellspacing="5" cellpadding="8">
 
-<<<<<<< HEAD
     <tr><td align="left"><b>Title</b></td>
     <td align="left"><b>Composer</b></td>
     <td align="left"><b>Performer Name</b></td>
     <td align="left"><b>Concert Date</b></td></tr>';
-=======
-    <tr><td align="left"><b>Work\'s Title</b></td>
-    <td align="left"><b>Work\'s Composer</b></td>
-    <td align="left"><b>Performance Title</b></td>
-    <td align="left"><b>Performance Date</b></td>
-    <td align="left"><b>Performer: Ensemble</b></td>
-	<td align="left"><b>Performer: Soloist</b></td></tr>';
-
->>>>>>> 27a2084de4cf1f7f5f937067d96ca0f46b53aaac
 
     // mysqli_fetch_array will return a row of data from the query
     // until no further data is available
@@ -45,13 +31,8 @@
       echo '<tr><td align="left">' . 
       $row['worksTitle'] . '</td><td align="left">' . 
       $row['worksComposer'] . '</td><td align="left">' .
-<<<<<<< HEAD
-=======
-      $row['performanceTitle'] . '</td><td align="left">' .
-      $row['concertDate'] . '</td><td align="left">' .
->>>>>>> 27a2084de4cf1f7f5f937067d96ca0f46b53aaac
       $row['ensembleName'] . '</td><td align="left">' .
-      $row['soloistName'] . '</td><td align="left">';
+      $row['concertDate'] . '</td><td align="left">';
 
       echo '</tr>';
     }
